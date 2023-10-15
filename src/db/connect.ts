139553e67f56f9dc7016eb5dc67808ce7c75ebdc
Ofir <ofir.zeitoun@gmail.com>
2023-10-15 21:17:58 +0300

    remove console of uri

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -4,10 +4,7 @@ export const connect = async () => {
   try {
     const uri = process.env.DBUri || "mongodb://127.0.0.1:27017";
     const res = await mongoose.connect(uri);
-    console.log(
-      "🚀 ~ file: connect.ts:9 ~ connect ~ mongoose: connected to ",
-      uri
-    );
+    console.log("🚀 ~ file: connect.ts:9 ~ connect ~ mongoose: connected");
     return res;
   } catch (error) {
     console.error("🚀 ~ file: connect.ts:11 ~ connect ~ error:", error);
